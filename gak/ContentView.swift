7327e8176cfb45b26f84709547cdd3c59921590e
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-05-31 14:54:28 -0400

    Initial Commit

---
+++ gak/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gak
+//
+//  Created by Beers,Theo on 5/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gak/gakApp.swift
@@ -0,0 +1,17 @@
+//
+//  gakApp.swift
+//  gak
+//
+//  Created by Beers,Theo on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct gakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
