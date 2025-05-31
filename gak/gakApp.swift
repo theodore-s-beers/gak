557435fedcd4c6d0ed6287cd47599895dc246ec2
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-05-31 15:17:09 -0400

    Format files

--- gak/gakApp.swift
+++ gak/gakApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct gakApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
